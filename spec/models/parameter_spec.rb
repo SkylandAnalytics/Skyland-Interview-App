97469a35904d6e97a5f3cb1803d153870fd359ae
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-21 17:37:53 -0600

    Create parameter, its validations and relationships, as well as process_step relationship

--- spec/models/parameter_spec.rb
+++ spec/models/parameter_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Parameter do
   describe '#validations' do
     it { should validate_presence_of(:process_step_id)}
-    it { should validate_presence_of(:measurment) }
-    it { should validate_inclusion_of(:measurment).in(-5.0..50.3) }
+    it { should validate_presence_of(:measurement) }
+    it { should validate_inclusion_of(:measurement).in_range(-5.0..50.3) }
   end
 
   describe '#relationships' do
