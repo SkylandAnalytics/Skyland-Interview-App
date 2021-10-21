f9213deb530ef3a13ae3bf1464aa3700602cdcb1
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-21 17:23:52 -0600

    Add foreign key validation test to Parameter

--- spec/models/parameter_spec.rb
+++ spec/models/parameter_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Parameter do
   describe '#validations' do
+    it { should validate_presence_of(:process_step_id)}
     it { should validate_presence_of(:measurment) }
     it { should validate_inclusion_of(:measurment).in(-5.0..50.3) }
   end
 
   describe '#relationships' do
-    it { should belong_to(:process_step)}
+    it { should belong_to(:process_step) }
   end
 end
