a97452f15aa9eb99db0072010465d69508162cb4
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-21 23:43:21 -0600

    Create test for measurement_greater_than_last

--- spec/models/process_step_spec.rb
+++ spec/models/process_step_spec.rb
@@ -18,4 +18,13 @@ RSpec.describe ProcessStep do
       expect(process_step.no_params?).to eq(true)
     end
   end
+
+  describe '#measurement_greater_than_last?' do
+    it 'returns true if given measurement is greater than its last associated measurement' do
+      process_step = ProcessStep.create!(name: 'First', description: 'Description of First step', position: 1)
+      process_step.parameters.create!(measurement: 4.7)
+
+      expect(process_step.measurement_greater_than_last(13.0)).to eq(true)
+    end
+  end
 end
