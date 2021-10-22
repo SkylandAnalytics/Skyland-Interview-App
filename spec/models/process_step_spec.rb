758c9bddfb63fb3a1cca1269efb0425dbdee8e26
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-21 23:38:28 -0600

    Create test for no params process step method

--- spec/models/process_step_spec.rb
+++ spec/models/process_step_spec.rb
@@ -6,8 +6,15 @@ RSpec.describe ProcessStep do
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:position) }
   end
-  
+
   describe '#relationships' do
     it { should have_many(:parameters) }
   end
+
+  describe '#no_params?' do
+    it 'returns true if a Process Step has no parameters' do
+      process_step = ProcessStep.create!(name: 'First', description: 'Description of First step', position: 1)
+
+      expect(process_step.no_params?).to eq(true)
+    end
 end
