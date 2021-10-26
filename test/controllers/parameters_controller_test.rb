da0ff51d2d587a7c75253443946fd0b4374ef07c
Kseniya K <kseniya.kudelich@gmail.com>
2021-10-26 15:16:53 -0600

    Add basic controller tests

---
+++ test/controllers/parameters_controller_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class ParametersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @process_step = FactoryBot.create(:process_step)
+    @parameter = Parameter.create(measurement: 1.2, process_step_id: @process_step.id)
+  end
+
+  def test_it_exists
+    assert_instance_of Parameter, @parameter
+  end
+
+  def test_it_has_attributes
+    assert_equal 1.2, @parameter.measurement
+  end
+end
--- test/factories/process_step.rb
+++ test/factories/process_step.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
    factory :process_step, class: ProcessStep do
      name { Faker::Science.element }
      description { Faker::Restaurant.description }
-     position { Faker::Number.between(from: -5, to: 50.3) }
+     position { Faker::Number.between(from: 1, to: 5) }
    end
  end
