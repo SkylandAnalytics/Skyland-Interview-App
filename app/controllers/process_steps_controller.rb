ca4b6e41766e986ecf1d1746636f3bd2ec346c01
Kseniya K <kseniya.kudelich@gmail.com>
2021-10-26 15:09:58 -0600

    Add create process step functionality

--- app/controllers/process_steps_controller.rb
+++ app/controllers/process_steps_controller.rb
@@ -3,6 +3,29 @@ class ProcessStepsController < ApplicationController
     @process_steps = ProcessStep.all
   end
 
+  def show
+  end
+
+  def new
+    @process_step = ProcessStep.new
+  end
+
+  def create
+    @process_step = ProcessStep.create(process_step_params)
+    if
+    else
+      @process_step.save
+    end
+
+    redirect_to process_steps_path
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
   def frontend
     @process_steps = ProcessStep.all
     respond_to do |format|
@@ -11,5 +34,11 @@ class ProcessStepsController < ApplicationController
       }
       format.html
     end
+  end
+
+  private
+
+  def process_step_params
+    params.require(:process_step).permit(:name, :description, :position)
   end
 end
