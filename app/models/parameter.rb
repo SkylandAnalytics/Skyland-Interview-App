d5245542e9426b1d3caa8969df89c44ea3ca1cdf
Kseniya K <kseniya.kudelich@gmail.com>
2021-10-26 15:17:11 -0600

    Cleanup models

--- app/models/parameter.rb
+++ app/models/parameter.rb
@@ -1,17 +1,8 @@
 class Parameter < ApplicationRecord
+
   belongs_to :process_step
 
   validates :measurement, presence: true
   validates :measurement, numericality: { :greater_than => -5.0 }
   validates :measurement, numericality: { :less_than => 50.3 }
-  # validate :maximum_parameter
-  # validates :measurement, numericality: { :less_than => maximum_parameters_validator }
-  #
-  # private
-  #
-  # def maximum_parameter
-  #   Parameter.find_by_sql
-  #     "SELECT MAX(measurement)
-  #      FROM parameters;"
-  # end
 end
