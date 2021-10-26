f9c6461d293ada444554d7593c7eb4f087d0db13
Kseniya K <kseniya.kudelich@gmail.com>
2021-10-26 11:54:21 -0600

    Add parameters routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
       get :frontend
     end
   end
+
+  resources :parameters do
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
