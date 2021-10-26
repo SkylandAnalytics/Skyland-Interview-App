daff32efc3d66c291c57ea8a5b7f0f1bdefa8f80
Kseniya K <kseniya.kudelich@gmail.com>
2021-10-26 15:08:00 -0600

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   resources :process_steps do
+    resources :parameters do
+    end
+
     collection do
       get :frontend
     end
   end
 
-  resources :parameters do
-  end
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
